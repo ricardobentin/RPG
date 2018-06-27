082bc3cf48e0fedd760b1783d8edcc635d6adc85
Nano Bacano <ricardo.bentin@gmail.com>
2018-06-27 16:00:56 -0400

    added some jquery for HP

---
+++ assets/javascript/game.js
@@ -0,0 +1,10 @@
+//initiatilize the game with HP for each player
+var bulbasaurHP = 120;
+var charmanderHP = 130;
+var horseaHP = 140;
+var pikachuHP = 150;
+//print HP to page
+$("#bulbasaurHP").text(bulbasaurHP);
+$("#charmanderHP").text(charmanderHP);
+$("#horseaHP").text(horseaHP);
+$("#pikachuHP").text(pikachuHP);
