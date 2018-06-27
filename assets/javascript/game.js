81c7baab992a0e7f9b2b7577b7bcf97091e7c53c
Nano Bacano <ricardo.bentin@gmail.com>
2018-06-27 17:08:42 -0400

    need to stop for a while

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,7 +4,14 @@ var charmanderHP = 130;
 var horseaHP = 140;
 var pikachuHP = 150;
 //print HP to page
-$("#bulbasaurHP").text(bulbasaurHP);
-$("#charmanderHP").text(charmanderHP);
-$("#horseaHP").text(horseaHP);
-$("#pikachuHP").text(pikachuHP);
+$("#bulbasaurHPBench").text(bulbasaurHP);
+$("#charmanderHPBench").text(charmanderHP);
+$("#horseaHPBench").text(horseaHP);
+$("#pikachuHPBench").text(pikachuHP);
+//if you click on any one of the characters, you need to move it to the yourCharacter div
+$(".cardBench").on("click", function () {
+    $(this).attr("hidden", "");
+    var firstClick = this;
+    if (this === firstClick)
+        $("#bulbasaurSelected").removeAttr("hidden");
+})
