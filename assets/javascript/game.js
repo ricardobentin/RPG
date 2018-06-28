0878177c2a0fb59c064764e88007551b32c36c3b
Nano Bacano <ricardo.bentin@gmail.com>
2018-06-28 16:29:36 -0400

    got a working selector!

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,17 +10,36 @@ $("#horseaHPBench").text(horseaHP);
 $("#pikachuHPBench").text(pikachuHP);
 //if you click on any one of the characters, you need to move it to the yourCharacter div
 $(".cardBench").on("click", function () {
-// why won't this syntax work :(
-    if (this === $("#bulbasaurBench.cardBench")) {
+    console.log(this);
+    // why won't this syntax work :(
+    if ($(this).attr("id") === "bulbasaurBench") {
         $("#bulbasaurSelected").removeAttr("hidden");
+        $("#bulbasaurBench").attr("hidden","");
+        $("#charmanderBench").attr("hidden","");
+        $("#horseaBench").attr("hidden","");
+        $("#pikachuBench").attr("hidden","");
+        
+        
     }
-    else if (this === "div#charmanderBench.cardBench"){
+    if ($(this).attr("id") === "charmanderBench") {
         $("#charmanderSelected").removeAttr("hidden");
+        $("#bulbasaurBench").attr("hidden","");
+        $("#charmanderBench").attr("hidden","");
+        $("#horseaBench").attr("hidden","");
+        $("#pikachuBench").attr("hidden","");
     }
-    else if (this === "div#horseaBench.cardBench"){
+    if ($(this).attr("id") === "horseaBench") {
         $("#horseaSelected").removeAttr("hidden");
+        $("#bulbasaurBench").attr("hidden","");
+        $("#charmanderBench").attr("hidden","");
+        $("#horseaBench").attr("hidden","");
+        $("#pikachuBench").attr("hidden","");
     }
-    else if (this === "div#pikachuBench.cardBench"){
+    if ($(this).attr("id") === "pikachuBench") {
         $("#pikachuSelected").removeAttr("hidden");
+        $("#bulbasaurBench").attr("hidden","");
+        $("#charmanderBench").attr("hidden","");
+        $("#horseaBench").attr("hidden","");
+        $("#pikachuBench").attr("hidden","");
     }
 })                                                              
