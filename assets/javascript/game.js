6cb8f0ca86fdc9214ed3203d5a2fa44cc9ee58b7
Nano Bacano <ricardo.bentin@gmail.com>
2018-06-28 16:47:12 -0400

    got the setup working! now need to write JS for battle sequence!

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,41 +5,84 @@ var horseaHP = 140;
 var pikachuHP = 150;
 //print HP to page
 $("#bulbasaurHPBench").text(bulbasaurHP);
+$("#bulbasaurHPSelected").text(bulbasaurHP);
+$("#bulbasaurHPEnemy").text(bulbasaurHP);
+$("#bulbasaurHPDefender").text(bulbasaurHP);
 $("#charmanderHPBench").text(charmanderHP);
+$("#charmanderHPSelected").text(charmanderHP);
+$("#charmanderHPEnemy").text(charmanderHP);
+$("#charmanderHPDefender").text(charmanderHP);
 $("#horseaHPBench").text(horseaHP);
+$("#horseaHPSelected").text(horseaHP);
+$("#horseaHPEnemy").text(horseaHP);
+$("#horseaHPDefender").text(horseaHP);
 $("#pikachuHPBench").text(pikachuHP);
+$("#pikachuHPSelected").text(pikachuHP);
+$("#pikachuHPEnemy").text(pikachuHP);
+$("#pikachuHPDefender").text(pikachuHP);
 //if you click on any one of the characters, you need to move it to the yourCharacter div
 $(".cardBench").on("click", function () {
-    console.log(this);
-    // why won't this syntax work :(
     if ($(this).attr("id") === "bulbasaurBench") {
         $("#bulbasaurSelected").removeAttr("hidden");
-        $("#bulbasaurBench").attr("hidden","");
-        $("#charmanderBench").attr("hidden","");
-        $("#horseaBench").attr("hidden","");
-        $("#pikachuBench").attr("hidden","");
-        
-        
+        $("#bulbasaurBench").attr("hidden", "");
+        $("#charmanderBench").attr("hidden", "");
+        $("#horseaBench").attr("hidden", "");
+        $("#pikachuBench").attr("hidden", "");
+        $("#bulbasaurEnemy").attr("hidden", "");
+
     }
     if ($(this).attr("id") === "charmanderBench") {
         $("#charmanderSelected").removeAttr("hidden");
-        $("#bulbasaurBench").attr("hidden","");
-        $("#charmanderBench").attr("hidden","");
-        $("#horseaBench").attr("hidden","");
-        $("#pikachuBench").attr("hidden","");
+        $("#bulbasaurBench").attr("hidden", "");
+        $("#charmanderBench").attr("hidden", "");
+        $("#horseaBench").attr("hidden", "");
+        $("#pikachuBench").attr("hidden", "");
+        $("#charmanderEnemy").attr("hidden", "");
     }
     if ($(this).attr("id") === "horseaBench") {
         $("#horseaSelected").removeAttr("hidden");
-        $("#bulbasaurBench").attr("hidden","");
-        $("#charmanderBench").attr("hidden","");
-        $("#horseaBench").attr("hidden","");
-        $("#pikachuBench").attr("hidden","");
+        $("#bulbasaurBench").attr("hidden", "");
+        $("#charmanderBench").attr("hidden", "");
+        $("#horseaBench").attr("hidden", "");
+        $("#pikachuBench").attr("hidden", "");
+        $("#horseaEnemy").attr("hidden", "");
     }
     if ($(this).attr("id") === "pikachuBench") {
         $("#pikachuSelected").removeAttr("hidden");
-        $("#bulbasaurBench").attr("hidden","");
-        $("#charmanderBench").attr("hidden","");
-        $("#horseaBench").attr("hidden","");
-        $("#pikachuBench").attr("hidden","");
+        $("#bulbasaurBench").attr("hidden", "");
+        $("#charmanderBench").attr("hidden", "");
+        $("#horseaBench").attr("hidden", "");
+        $("#pikachuBench").attr("hidden", "");
+        $("#pikachuEnemy").attr("hidden", "");
     }
-})                                                              
+})
+$(".cardEnemyBench").on("click", function () {
+    if ($(this).attr("id") === "bulbasaurEnemy") {
+        $("#bulbasaurDefender").removeAttr("hidden");
+        $("#bulbasaurEnemy").attr("hidden", "");
+        $("#charmanderEnemy").attr("hidden", "");
+        $("#horseaEnemy").attr("hidden", "");
+        $("#pikachuEnemy").attr("hidden", "");
+    }
+    if ($(this).attr("id") === "charmanderEnemy") {
+        $("#charmanderDefender").removeAttr("hidden");
+        $("#bulbasaurEnemy").attr("hidden", "");
+        $("#charmanderEnemy").attr("hidden", "");
+        $("#horseaEnemy").attr("hidden", "");
+        $("#pikachuEnemy").attr("hidden", "");
+    }
+    if ($(this).attr("id") === "horseaEnemy") {
+        $("#horseaDefender").removeAttr("hidden");
+        $("#bulbasaurEnemy").attr("hidden", "");
+        $("#charmanderEnemy").attr("hidden", "");
+        $("#horseaEnemy").attr("hidden", "");
+        $("#pikachuEnemy").attr("hidden", "");
+    }
+    if ($(this).attr("id") === "pikachuEnemy") {
+        $("#pikachuDefender").removeAttr("hidden");
+        $("#bulbasaurEnemy").attr("hidden", "");
+        $("#charmanderEnemy").attr("hidden", "");
+        $("#horseaEnemy").attr("hidden", "");
+        $("#pikachuEnemy").attr("hidden", "");
+    }
+})                                                     
